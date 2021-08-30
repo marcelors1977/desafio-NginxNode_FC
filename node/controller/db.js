2a4c05eedde0f30abd20864d4e5bc0f00287e963
Marcelo <marcelo.rds1977@gmail.com>
2021-08-29 23:09:59 -0300

    perf: :alien: Alterando para buscar variável de ambiente
    alterando código para buscar parâmetros de ambiente para conexão em banco de dados

--- node/controller/db.js
+++ node/controller/db.js
@@ -1,9 +1,12 @@
 const mysql = require('mysql')
+const db_name = process.env.db_name
+const user_db = process.env.user_db
+const pass_db = process.env.pass_db
 
 const config = {
-    host: 'db',
-    user: 'root',
-    password: 'root',
+    host: db_name,
+    user: user_db,
+    password: pass_db,
     database:'nodedb'
 }
 
@@ -22,14 +25,20 @@ module.exports = {
             })
     },
     setPeople: () => {
-        const sqlInsert = [ `INSERT INTO people(name) values('Wesley Willians');`,
-                    `INSERT INTO people(name) values('Luiz Carlos');`,
-                    `INSERT INTO people(name) values('Guilherme Ferreira');`,
-                    `INSERT INTO people(name) values('Leonan Luppi');`,
-                    `INSERT INTO people(name) values('Marcelo Rodrigues');`]
-                    
-        sqlInsert.forEach(row => {
-            connection.query(row)
+        connection.query(`SELECT COUNT(1) FROM people`, (err,result) => {
+            if (err) throw err
+
+            if (result[0].total === 0 ){
+                const sqlInsert = [ `INSERT INTO people(name) values('Wesley Willians');`,
+                `INSERT INTO people(name) values('Luiz Carlos');`,
+                `INSERT INTO people(name) values('Guilherme Ferreira');`,
+                `INSERT INTO people(name) values('Leonan Luppi');`,
+                `INSERT INTO people(name) values('Marcelo Rodrigues');`]
+                
+                sqlInsert.forEach(row => {
+                    connection.query(row)
+                })    
+            }
         })
     }
 }
